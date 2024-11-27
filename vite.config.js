8ec4472dea2c8c8ba62c00511e67db83fa827e92
Geoo Chi <geoooochi@gmail.com>
2024-11-27 13:54:55 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        ok: resolve(__dirname, 'ok.html'),
+        buggy: resolve(__dirname, 'buggy.html'),
+      },
+    },
+  },
 })
